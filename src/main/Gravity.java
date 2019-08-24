c362a175b2037ac99d6d74766a9b4ad3b57ffb1b
himanshu1323235 <himanshu,gautam_cs18@gla.ac.in>
2019-08-24 12:51:23 +0530

    Calculate the result and print it to the screen.

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -7,6 +7,8 @@ package main;
 // import java.util.Scanner;
 // uncomment the above line to use input in your program.
 
+import java.util.Scanner;
+
 public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
@@ -15,12 +17,19 @@ public class Gravity {
         double distance;
         double speed;
         double gravity = 9.8;
+        Scanner s = new Scanner(System.in);
+        time = s.nextDouble();
+        speed = gravity * time;
+        distance = 0.5 * gravity * (time * time);
+        System.out.printf(" The speed of the object at " + time + " seconds after its release is" + speed + " and the distance the object has travelled in the" + time + " seconds after the relase is" + distance);
+
+
 
 
 		/* TODO 2:
          *   a.
          *   b.
-		 *   c. Calculate the result and print it to the screen.
+         *   c.
 		 * */
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
